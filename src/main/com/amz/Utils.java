a04409db85e2699dfe0aa661bcf1e19ae6542213
dasa <Amresh.Das@SincroDigital.com>
2021-08-20 21:27:59 +0530

    New solutions

--- src/main/com/amz/Utils.java
+++ src/main/com/amz/Utils.java
@@ -2,6 +2,7 @@ package com.amz;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Utils {
@@ -13,5 +14,20 @@ public class Utils {
 
     public static String listOfIntegerListToString(final List<List<Integer>> output) {
         return "[" + output.stream().map(i -> "[" + i.stream().map(Object::toString).collect(Collectors.joining(",")) + "]").collect(Collectors.joining(",")) + "]";
+    }
+
+    public static int[][] to2dIntMatrix(final String input) {
+        final String[] rows = input.substring(1, input.length() - 2).split("\\],\\[");
+        final int colLen = rows[0].split("\\],\\[").length;
+        final int[][] out = new int[rows.length][colLen];
+        int i = 0;
+        for (String row : rows) {
+            out[i++] = stringToIntArray(row);
+        }
+        return out;
+    }
+
+    public static String from2dIntMatrix(final int[][] input) {
+        return "[" + Arrays.stream(input).map(i -> "[" + Arrays.stream(i).boxed().map(Objects::toString).collect(Collectors.joining(",")) + "]").collect(Collectors.joining(",")) + "]";
     }
 }
