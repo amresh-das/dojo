40d09f07532ca83d2c550196f29e8cd4d21fc446
askds <askdsden@gmail.com>
2022-07-09 02:09:31 +0530

    Add problems

--- src/test/com/amz/PaintHouseIII.java
+++ src/test/com/amz/PaintHouseIII.java
@@ -8,13 +8,18 @@ import java.util.*;
 public class PaintHouseIII {
     public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
         final Map<String, Integer> memo = new HashMap<>();
-        int minCost = computeCombinations(houses, cost, m, n, 0, target, 0, -1, memo, 0, "");
+        int minCost = computeCombinations(houses, cost, m, n, 0, target, 0, -1, memo, 0);
         return minCost == Integer.MAX_VALUE ? -1 : minCost;
     }
 
-    private int computeCombinations(final int[] houses, final int[][] cost, final int m, final int n, final int position, final int target, final int neighbourHoodCount, final int prevColor, final Map<String, Integer> memo, final int currentCost, final String key) {
+    private int computeCombinations(final int[] houses, final int[][] cost, final int m, final int n, final int position, final int target, final int neighbourHoodCount, final int prevColor, final Map<String, Integer> memo, final int currentCost) {
+        final String key = toKey(prevColor, neighbourHoodCount, position, currentCost);
         if (memo.containsKey(key)) {
             return memo.get(key);
+        }
+        if (neighbourHoodCount > target) {
+            memo.put(key, Integer.MAX_VALUE);
+            return Integer.MAX_VALUE;
         }
         if (position == m) {
             if (target == neighbourHoodCount) {
@@ -28,20 +33,20 @@ public class PaintHouseIII {
             int min = Integer.MAX_VALUE;
             for (int i = 1; i <= n; i++) {
                 int neighbourHoods = neighbourHoodCount + (prevColor == i ? 0 : 1);
-                min = Math.min(min, computeCombinations(houses, cost, m, n, position + 1, target, neighbourHoods, i, memo, currentCost + cost[position][i - 1], toKey(i, neighbourHoods, position + 1)));
+                min = Math.min(min, computeCombinations(houses, cost, m, n, position + 1, target, neighbourHoods, i, memo, currentCost + cost[position][i - 1]));
             }
             memo.put(key, min);
             return min;
         } else {
             int neighbourHoods = neighbourHoodCount + (position == 0 ? 1 : prevColor == houses[position] ? 0 : 1);
-            int result = computeCombinations(houses, cost, m, n, position + 1, target, neighbourHoods, houses[position], memo, currentCost, toKey(houses[position], neighbourHoods, position + 1));
+            int result = computeCombinations(houses, cost, m, n, position + 1, target, neighbourHoods, houses[position], memo, currentCost);
             memo.put(key, result);
             return result;
         }
     }
 
-    private String toKey(final int color, final int neighbourHoods, final int position) {
-        return color + "." + neighbourHoods + "." + position;
+    private String toKey(final int color, final int neighbourHoods, final int position, final int cost) {
+        return color + "." + neighbourHoods + "." + position + "." + cost;
     }
 
     @Test
@@ -81,7 +86,17 @@ public class PaintHouseIII {
         int m = 100;
         int n = 20;
         int target = 100;
-        Assertions.assertEquals(-1, minCost(Utils.stringToIntArray(houses), Utils.to2dIntMatrix(costs), m, n, target));
+        Assertions.assertEquals(1000000, minCost(Utils.stringToIntArray(houses), Utils.to2dIntMatrix(costs), m, n, target));
+    }
+
+    @Test
+    public void check5() {
+        String houses = "[0,6,0,0,0,0,1,0,2,4,0,3,0,3,0,0,6,0,0,1,0,8,0,10,0,0,0,10,7]";
+        String costs = "[[24,16,20,39,47,30,7,50,26,39],[47,1,1,11,14,8,36,18,10,10],[7,19,15,29,16,29,19,36,31,35],[46,5,24,47,8,4,26,36,46,42],[33,48,15,21,34,23,35,40,48,39],[20,28,37,6,12,7,4,6,40,39],[31,22,15,43,40,21,11,26,43,26],[1,5,25,7,18,15,19,49,13,18],[25,28,30,43,27,48,34,30,25,32],[50,13,2,24,31,38,16,19,2,49],[21,26,2,50,44,6,1,4,5,34],[7,4,12,26,5,19,33,41,13,3],[19,12,31,32,26,9,28,40,32,11],[24,15,35,1,9,4,6,34,14,6],[4,43,50,5,38,20,26,14,2,23],[6,26,37,24,25,16,9,19,24,28],[13,23,33,46,13,47,6,47,8,3],[31,13,24,35,37,6,40,13,14,34],[39,13,47,37,36,47,48,9,10,22],[11,44,23,23,13,38,37,44,36,13],[19,24,49,14,1,10,49,49,1,37],[41,45,46,48,48,16,47,25,2,5],[14,20,8,27,41,16,34,30,5,16],[48,6,43,23,21,29,37,23,48,29],[29,16,37,5,20,1,37,25,6,19],[44,30,7,37,4,20,48,45,1,39],[19,39,12,41,45,46,22,15,4,20],[40,11,39,47,25,26,24,20,5,7],[50,47,35,48,44,50,15,40,37,33]]";
+        int m = 29;
+        int n = 10;
+        int target = 22;
+        Assertions.assertEquals(97, minCost(Utils.stringToIntArray(houses), Utils.to2dIntMatrix(costs), m, n, target));
     }
 
 }
