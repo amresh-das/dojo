6b3abd1676a3057b25512341ca69daec7bf59cac
Amresh Das <mail2amreshdas@gmail.com>
2022-07-22 18:16:50 +0530

    Add Problems

--- src/test/com/amz/SlidingPuzzle.java
+++ src/test/com/amz/SlidingPuzzle.java
@@ -7,75 +7,77 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * TODO
  * @see "https://leetcode.com/problems/sliding-puzzle/"
  */
 public class SlidingPuzzle {
     private final int[][] target = new int[][] {new int[] {1,2,3}, new int[] {4,5,0}};
 
     public int slidingPuzzle(int[][] board) {
-        final Map<String, Integer> visited = new HashMap<>();
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == 0) {
-                    int moves = slide(board, i, j, toString(target), visited, 0);
-                    return moves == Integer.MAX_VALUE ? -1 : moves;
-                }
-            }
+        final String srcStr = toString(board);
+        final String tgtStr = toString(target);
+        final Set<String> visited = new HashSet<>();
+        final PriorityQueue<Object[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> (int) a[0]));
+        pq.offer(new Object[] {0, srcStr});
+
+        while (!pq.isEmpty()) {
+            Object[] next = pq.poll();
+            String src = (String) next[1];
+            if (visited.contains(src)) continue;
+            visited.add(src);
+            int level = (int) next[0];
+            if (src == null) continue;
+            if (src.equals(tgtStr)) return level;
+
+            pq.offer(new Object[] {level + 1, up(src)});
+            pq.offer(new Object[] {level + 1, down(src)});
+            pq.offer(new Object[] {level + 1, left(src)});
+            pq.offer(new Object[] {level + 1, right(src)});
         }
         return -1;
     }
 
-    private int slide(int[][] board, int x, int y, String target, Map<String, Integer> visited, int moves) {
-        if (x < 0 || y < 0 || x >= board.length || y >= board[0].length) return -1;
-        String str = toString(board);
-        if (str.equals(target)) return 0;
-        if (visited.containsKey(str)) return Integer.MAX_VALUE;
-        int min = Integer.MAX_VALUE;
-
-        //slide left
-        if (y > 0) {
-            int temp = board[x][y - 1];
-            board[x][y] = temp;
-            board[x][y - 1] = 0;
-            min = Math.min(min, slide(board, x, y - 1, target, visited, moves));
-            board[x][y - 1] = temp;
-            board[x][y] = 0;
+    private String up(String src) {
+        int index = src.indexOf('0');
+        if (index > 2) {
+            char[] t = src.toCharArray();
+            t[3 + index % 3] = t[index % 3];
+            t[index % 3] = '0';
+            return new String(t);
         }
+        return null;
+    }
 
-        //slide right
-
-        if (y < board[0].length - 1) {
-            int temp = board[x][y + 1];
-            board[x][y] = temp;
-            board[x][y + 1] = 0;
-            min = Math.min(min, slide(board, x, y + 1, target, visited, moves));
-            board[x][y + 1] = temp;
-            board[x][y] = 0;
+    private String down(String src) {
+        int index = src.indexOf('0');
+        if (index < 3) {
+            char[] t = src.toCharArray();
+            t[index % 3] = t[3 + index % 3];
+            t[3 + index % 3] = '0';
+            return new String(t);
         }
+        return null;
+    }
 
-        //slide up
-        if (x > 0) {
-            int temp = board[x - 1][y];
-            board[x][y] = temp;
-            board[x - 1][y] = 0;
-            min = Math.min(min, slide(board, x - 1, y, target, visited, moves));
-            board[x - 1][y] = temp;
-            board[x][y] = 0;
+    private String left(String src) {
+        int index = src.indexOf('0');
+        if (index % 3 > 0) {
+            char[] t = src.toCharArray();
+            t[index] = t[index - 1];
+            t[index - 1] = '0';
+            return new String(t);
         }
+        return null;
+    }
 
-        //slide down
-        if (x < board.length - 1) {
-            int temp = board[x + 1][y];
-            board[x][y] = temp;
-            board[x + 1][y] = 0;
-            min = Math.min(min, slide(board, x + 1, y, target, visited, moves));
-            board[x + 1][y] = temp;
-            board[x][y] = 0;
+    private String right(String src) {
+        int index = src.indexOf('0');
+        if (index % 3 < 2) {
+            char[] t = src.toCharArray();
+            t[index] = t[index + 1];
+            t[index + 1] = '0';
+            return new String(t);
         }
-
-        visited.put(str, min);
-        return moves + min;
+        return null;
     }
 
     private String toString(int[][] board) {
