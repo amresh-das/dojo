8316f87d0cfd536d708497ec120de318bfa34a15
Amresh Das <mail2amreshdas@gmail.com>
2022-09-03 20:00:38 +0530

    Add Problems

--- src/test/com/amz/NumbersWithSameConsecutiveDifferences.java
+++ src/test/com/amz/NumbersWithSameConsecutiveDifferences.java
@@ -3,10 +3,8 @@ package com.amz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @see "https://leetcode.com/problems/numbers-with-same-consecutive-differences/"
@@ -14,28 +12,27 @@ import java.util.PriorityQueue;
 public class NumbersWithSameConsecutiveDifferences {
 
     public int[] numsSameConsecDiff(int n, int k) {
-        List<Integer> ans = new ArrayList<>();
+        final List<Integer> ans = new ArrayList<>();
         if (n != 0) {
-            final PriorityQueue<Object[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> (int) o[0]));
-            for (Integer i = 1; i < 10; i++) {
-                pq.offer(new Object[]{1, i.toString(), i});
+            final PriorityQueue<String> pq = new PriorityQueue<>(Comparator.comparingInt(String::length));
+            for (char c = '1'; c <= '9'; c++) {
+                pq.offer("" + c);
             }
             while (!pq.isEmpty()) {
-                Object[] obj = pq.poll();
-                int digitCount = (int) obj[0];
-                String num = (String) obj[1];
-                if (digitCount == n) {
+                String num = pq.poll();
+                int size = num.length();
+                if (size == n) {
                     ans.add(Integer.parseInt(num));
                 } else {
-                    int lastDigit = (int) obj[2];
+                    int lastDigit = num.charAt(size - 1) - '0';
                     int optA = lastDigit + k;
                     if (optA < 10) {
-                        pq.offer(new Object[]{digitCount + 1, num + optA, optA});
+                        pq.offer(num + optA);
                     }
                     if (k > 0) {
                         int optB = lastDigit - k;
                         if (optB >= 0) {
-                            pq.offer(new Object[]{digitCount + 1, num + optB, optB});
+                            pq.offer(num + optB);
                         }
                     }
                 }
@@ -46,17 +43,21 @@ public class NumbersWithSameConsecutiveDifferences {
 
     @Test
     public void check1() {
-        Assertions.assertEquals("[181,292,707,818,929]", Utils.toString(numsSameConsecDiff(3, 7)));
+        verify(3, 7, 181,292,707,818,929);
     }
 
     @Test
     public void check2() {
-        Assertions.assertEquals("[10,12,21,23,32,34,43,45,54,56,65,67,76,78,87,89,98]", Utils.toString(numsSameConsecDiff(2, 1)));
+        verify(2, 1, 10,12,21,23,32,34,43,45,54,56,65,67,76,78,87,89,98);
     }
 
     @Test
     public void check3() {
-        Assertions.assertEquals("[11,22,33,44,55,66,77,88,99]", Utils.toString(numsSameConsecDiff(2, 0)));
+        verify(2, 0, 11,22,33,44,55,66,77,88,99);
+    }
+
+    private void verify(int n, int k, int... expected) {
+        Assertions.assertEquals(Arrays.stream(expected).boxed().collect(Collectors.toSet()), Arrays.stream(numsSameConsecDiff(n, k)).boxed().collect(Collectors.toSet()));
     }
 
 }
